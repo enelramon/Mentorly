77e707b3761dce1c0f18b29a20a4a3cb4108d390
jorge <jorge@LAPTOP-D8GDLQRL>
2026-08-02 17:16:04 -0400

    Agrega la funcion trim para eliminar espacios del final y de delante del email

--- app/src/main/java/com/sagrd/mentorly/domain/student/usecase/StudentValidations.kt
+++ app/src/main/java/com/sagrd/mentorly/domain/student/usecase/StudentValidations.kt
@@ -8,7 +8,7 @@ data class StudentValidation(
 fun validateEmail(email: String): StudentValidation {
     return when {
         email.isBlank() -> StudentValidation(false, "Email requerido")
-        !email.contains("@") -> StudentValidation(false, "Debe contener '@'")
+        !email.trim().contains("@") -> StudentValidation(false, "Debe contener '@'")
         else -> StudentValidation(true)
     }
 }
