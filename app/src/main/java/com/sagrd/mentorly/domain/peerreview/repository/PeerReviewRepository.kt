17de587ee48087c77d5cd4ef0d58b645244063b4
jorge <jorge@LAPTOP-D8GDLQRL>
2026-08-02 16:31:18 -0400

    Agrega las funciones correspondientes a ambos repositorios pero comentados porque faltan cosas

---
+++ app/src/main/java/com/sagrd/mentorly/domain/peerreview/repository/PeerReviewRepository.kt
@@ -0,0 +1,8 @@
+package com.sagrd.mentorly.domain.peerreview.repository
+
+import com.sagrd.mentorly.data.remote.Resource
+import kotlinx.coroutines.flow.Flow
+
+interface PeerReviewRepository {
+    //fun submitReview(submissionId: String, reviewerStudentId: String, isApproved: Boolean, feedbackComment: String): Flow<Resource<PeerReviewResult>>
+}
---
+++ app/src/main/java/com/sagrd/mentorly/domain/student/repository/StudentEnrollmentRepository.kt
@@ -0,0 +1,14 @@
+package com.sagrd.mentorly.domain.student.repository
+
+import com.sagrd.mentorly.data.remote.Resource
+import kotlinx.coroutines.flow.Flow
+
+interface StudentEnrollmentRepository {
+//    fun enroll(studentId: String, courseId: String): Flow<Resource<EnrollmentResult>>
+//    fun getEnrollmentStatus(enrollmentId: String): Flow<Resource<EnrollmentStatus>>
+//    fun submitExercise(
+//        enrollmentId: String,
+//        activityId: String,
+//        evidenceUrl: String
+//    ): Flow<Resource<SubmissionResult>>
+}
