1cdac732eeaba7de47575ead56d4254163e20a0d
jorge <jorge@LAPTOP-D8GDLQRL>
2026-08-03 19:47:32 -0400

    Cambia los parametros del usecase

--- app/src/main/java/com/sagrd/mentorly/domain/usecase/peerreview/UpdatePeerReviewUseCase.kt
+++ app/src/main/java/com/sagrd/mentorly/domain/usecase/peerreview/UpdatePeerReviewUseCase.kt
@@ -6,6 +6,6 @@ import jakarta.inject.Inject
 class UpdatePeerReviewUseCase @Inject constructor(
     private val repository: PeerReviewRepository
 ) {
-    operator fun invoke(id: String, submissionId: String, isApproved: Boolean, feedbackComment: String) =
-        repository.updatePeerReview(id, submissionId, isApproved, feedbackComment)
+    operator fun invoke(id: String, isApproved: Boolean, feedbackComment: String) =
+        repository.updatePeerReview(id, isApproved, feedbackComment)
 }
