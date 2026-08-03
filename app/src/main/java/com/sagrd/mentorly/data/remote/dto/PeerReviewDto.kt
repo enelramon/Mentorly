13330bd469eb354e10ea45e690be80d1ca9b31be
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-08-03 11:29:26 -0400

    Crea DTOs de Peer Reviews

---
+++ app/src/main/java/com/sagrd/mentorly/data/remote/dto/PeerReviewDto.kt
@@ -0,0 +1,23 @@
+package com.sagrd.mentorly.data.remote.dto
+
+import com.sagrd.mentorly.domain.mentorly.enum.SubmissionStatus
+
+data class CreatePeerReviewRequestDto (
+    val submissionId: String,
+    val reviewerStudentId: String,
+    val isApproved: Boolean,
+    val feedbackComment: String,
+    val createdAtUtc: String
+)
+
+data class PeerReviewResultDto (
+    val peerReviewId: String,
+    val submissionId: String,
+    val reviewerStudentId: String,
+    val isApproved: Boolean,
+    val feedbackComment: String,
+    val createdAtUtc: String,
+    val positiveReviews: Int,
+    val requiredPositiveReviews: Int,
+    val submissionStatus: SubmissionStatus
+)
