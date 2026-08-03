823f0e3299707522a381adae6569a9c57bf924e1
jorge <jorge@LAPTOP-D8GDLQRL>
2026-08-03 19:45:36 -0400

    Cambia la version a 24 para no tener que importar el requiresApi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
 
     defaultConfig {
         applicationId = "com.sagrd.mentorly"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 37
         versionCode = 1
         versionName = "1.0"
