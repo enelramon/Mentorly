fd702c0178f68819cf289fc9ee5313e2ec2c772e
jorge <jorge@LAPTOP-D8GDLQRL>
2026-08-03 19:01:28 -0400

    Agrega el mapeo de los dtos

--- app/src/main/java/com/sagrd/mentorly/data/remote/dto/PeerReviewDto.kt
+++ app/src/main/java/com/sagrd/mentorly/data/remote/dto/PeerReviewDto.kt
@@ -1,6 +1,8 @@
 package com.sagrd.mentorly.data.remote.dto
 
 import com.sagrd.mentorly.domain.enum.SubmissionStatus
+import com.sagrd.mentorly.domain.model.PeerReview
+import com.sagrd.mentorly.domain.model.PeerReviewResult
 
 data class CreatePeerReviewRequestDto (
     val submissionId: String,
@@ -20,4 +22,36 @@ data class PeerReviewResultDto (
     val positiveReviews: Int,
     val requiredPositiveReviews: Int,
     val submissionStatus: SubmissionStatus
-)
+) {
+    fun toDomain(): PeerReviewResult {
+        return PeerReviewResult(
+            peerReviewId = peerReviewId,
+            submissionId = submissionId,
+            reviewerStudentId = reviewerStudentId,
+            isApproved = isApproved,
+            feedbackComment = feedbackComment,
+            createdAtUtc = createdAtUtc,
+            positiveReviews = positiveReviews,
+            requiredPositiveReviews = requiredPositiveReviews,
+            submissionStatus = submissionStatus
+        )
+    }
+}
+
+data class PeerReviewDto(
+    val id: String,
+    val submissionId: String,
+    val isApproved: Boolean,
+    val feedbackComment: String,
+    val createdAt: String
+) {
+    fun toDomain(): PeerReview {
+        return PeerReview(
+            id = id,
+            submissionId = submissionId,
+            isApproved = isApproved,
+            feedbackComment = feedbackComment,
+            createdAt = createdAt
+        )
+    }
+}
