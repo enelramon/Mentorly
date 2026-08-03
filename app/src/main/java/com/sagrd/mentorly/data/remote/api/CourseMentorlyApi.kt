e24d63459cf81cc38792da9327cb30c9af0219d2
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-08-03 16:34:14 -0400

    Cambia nombre de interfaz MentorlyApi a CourseMentorlyApi

---
+++ app/src/main/java/com/sagrd/mentorly/data/remote/api/CourseMentorlyApi.kt
@@ -0,0 +1,38 @@
+package com.sagrd.mentorly.data.remote.api
+
+import com.sagrd.mentorly.data.remote.dto.CourseDto
+import com.sagrd.mentorly.data.remote.dto.CreateCourseDto
+import com.sagrd.mentorly.data.remote.dto.UpdateCourseDto
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface CourseMentorlyApi {
+    @GET("api/Courses")
+    suspend fun getCourses(): Response<List<CourseDto>>
+
+    @GET("api/Courses/{id}")
+    suspend fun getCourseDetail(
+        @Path("id") id: String
+    ): Response<CourseDto>
+
+    @POST("api/Courses")
+    suspend fun createCourse(
+        @Body dto: CreateCourseDto
+    ): Response<CourseDto>
+
+    @PUT("api/Courses/{id}")
+    suspend fun updateCourse(
+        @Path("id") id: String,
+        @Body dto: UpdateCourseDto
+    ): Response<Unit>
+
+    @DELETE("api/Courses/{id}")
+    suspend fun deleteCourse(
+        @Path("id") id: String
+    ): Response<Unit>
+}
