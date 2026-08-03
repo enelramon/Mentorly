57481c19ba3ac6a7e650695620a3483913f57dc9
jorge <jorge@LAPTOP-D8GDLQRL>
2026-08-03 16:21:22 -0400

    Modifica los repositorios de estudiante y PeerReviewRepository.kt

--- app/src/main/java/com/sagrd/mentorly/domain/repository/PeerReviewRepository.kt
+++ app/src/main/java/com/sagrd/mentorly/domain/repository/PeerReviewRepository.kt
@@ -1,9 +1,19 @@
 package com.sagrd.mentorly.domain.repository
 
 import com.sagrd.mentorly.data.remote.Resource
+import com.sagrd.mentorly.domain.model.PeerReview
 import com.sagrd.mentorly.domain.model.PeerReviewResult
 import kotlinx.coroutines.flow.Flow
 
 interface PeerReviewRepository {
-    fun submitReview(submissionId: String, reviewerStudentId: String, isApproved: Boolean, feedbackComment: String): Flow<Resource<PeerReviewResult>>
+    fun getPeerReviews(): Flow<Resource<List<PeerReview>>>
+    fun getPeerReview(id: String): Flow<Resource<PeerReview>>
+    fun submitReview(
+        submissionId: String,
+        reviewerStudentId: String,
+        isApproved: Boolean,
+        feedbackComment: String
+    ): Flow<Resource<PeerReviewResult>>
+    fun updatePeerReview(id: String, isApproved: Boolean, feedbackComment: String): Flow<Resource<Boolean>>
+    fun deletePeerReview(id: String): Flow<Resource<Boolean>>
 }
--- app/src/main/java/com/sagrd/mentorly/domain/repository/StudentRepository.kt
+++ app/src/main/java/com/sagrd/mentorly/domain/repository/StudentRepository.kt
@@ -7,5 +7,7 @@ import kotlinx.coroutines.flow.Flow
 interface StudentRepository {
     fun getStudents(): Flow<Resource<List<Student>>>
     fun getStudent(id: String): Flow<Resource<Student>>
-    fun updateProfile(id: String, email: String, displayName: String): Flow<Resource<Student>>
+    fun createStudent(googleUserId: String, email: String, displayName: String): Flow<Resource<Student>>
+    fun updateStudent(id: String, email: String, displayName: String): Flow<Resource<Boolean>>
+    fun deleteStudent(id: String): Flow<Resource<Boolean>>
 }
