a57ce6691998158a228545ea6ca4fd5978106a30
jorge <jorge@LAPTOP-D8GDLQRL>
2026-08-03 14:04:38 -0400

    Importa el modelo para utilizarlo en el metodo abstracto (arreglando el error)

--- app/src/main/java/com/sagrd/mentorly/domain/repository/PeerReviewRepository.kt
+++ app/src/main/java/com/sagrd/mentorly/domain/repository/PeerReviewRepository.kt
@@ -1,6 +1,7 @@
 package com.sagrd.mentorly.domain.repository
 
 import com.sagrd.mentorly.data.remote.Resource
+import com.sagrd.mentorly.domain.model.PeerReviewResult
 import kotlinx.coroutines.flow.Flow
 
 interface PeerReviewRepository {
