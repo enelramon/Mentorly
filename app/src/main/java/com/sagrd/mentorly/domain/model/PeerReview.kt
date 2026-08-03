ed51ebb399fe8357e6340655eb8eeac11563d9b0
jorge <jorge@LAPTOP-D8GDLQRL>
2026-08-03 19:35:46 -0400

    Regresa el atributo reviewerStudentId llevandonos del dto

--- app/src/main/java/com/sagrd/mentorly/domain/model/PeerReview.kt
+++ app/src/main/java/com/sagrd/mentorly/domain/model/PeerReview.kt
@@ -3,6 +3,7 @@ package com.sagrd.mentorly.domain.model
 data class PeerReview(
     val id: String,
     val submissionId: String,
+    val reviewerStudentId: String,
     val isApproved: Boolean,
     val feedbackComment: String,
     val createdAt: String
