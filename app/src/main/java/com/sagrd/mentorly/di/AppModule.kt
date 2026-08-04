7101584896907599c4f0d0614753796ace856ca2
jorge <jorge@LAPTOP-D8GDLQRL>
2026-08-03 21:06:59 -0400

    Provee la api de PeerReview y provee la implementacion del repositorio

--- app/src/main/java/com/sagrd/mentorly/di/AppModule.kt
+++ app/src/main/java/com/sagrd/mentorly/di/AppModule.kt
@@ -1,10 +1,14 @@
 package com.sagrd.mentorly.di
 
 import com.sagrd.mentorly.data.remote.api.CourseMentorlyApi
+import com.sagrd.mentorly.data.remote.api.PeerReviewMentorlyApi
 import com.sagrd.mentorly.data.remote.api.SubmissionMentorlyApi
 import com.sagrd.mentorly.data.remote.dto.EnumMoshiAdapters
+import com.sagrd.mentorly.data.remote.remotedatasource.PeerReviewRemoteDataSource
 import com.sagrd.mentorly.data.remote.remotedatasource.SubmissionRemoteDataSource
+import com.sagrd.mentorly.data.repository.PeerReviewRepositoryImpl
 import com.sagrd.mentorly.data.repository.SubmissionRepositoryImpl
+import com.sagrd.mentorly.domain.repository.PeerReviewRepository
 import com.sagrd.mentorly.domain.repository.SubmissionRepository
 import com.squareup.moshi.KotlinJsonAdapterFactory
 import com.squareup.moshi.Moshi
@@ -53,7 +57,24 @@ object AppModule {
 
     @Provides
     @Singleton
+    fun providePeerReviewApi(moshi: Moshi): PeerReviewMentorlyApi {
+        val baseUrl = "https://mentorlyapi-ap2-f8gfgwh3efchgzfn.eastus2-01.azurewebsites.net/"
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(PeerReviewMentorlyApi::class.java)
+    }
+
+    @Provides
+    @Singleton
     fun provideSubmissionRepository(remoteDataSource: SubmissionRemoteDataSource): SubmissionRepository {
         return SubmissionRepositoryImpl(remoteDataSource)
+    }
+
+    @Provides
+    @Singleton
+    fun providePeerReviewRepository(remoteDataSource: PeerReviewRemoteDataSource): PeerReviewRepository {
+        return PeerReviewRepositoryImpl(remoteDataSource)
     }
 }
