faeade64d1397762b0188b17f6c3e89204f7feba
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-08-03 20:25:03 -0400

    Agrega AuthScreen en el MainActivity para depuracion

--- app/src/main/java/com/sagrd/mentorly/MainActivity.kt
+++ app/src/main/java/com/sagrd/mentorly/MainActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.sagrd.mentorly.presentation.login.AuthScreen
 import com.sagrd.mentorly.ui.theme.MentorlyTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MentorlyTheme {
+                AuthScreen()
             }
         }
     }
