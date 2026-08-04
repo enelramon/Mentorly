0a8d8e847cd238910f0f7fb24ee68e78ad9eb918
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-08-03 21:13:31 -0400

    Mueve Provides del Firebase a FirebaseModule

--- app/src/main/java/com/sagrd/mentorly/di/AppModule.kt
+++ app/src/main/java/com/sagrd/mentorly/di/AppModule.kt
@@ -1,20 +1,27 @@
 package com.sagrd.mentorly.di
 
+import android.content.Context
+import androidx.credentials.CredentialManager
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.sagrd.mentorly.data.remote.api.CourseMentorlyApi
-import com.sagrd.mentorly.data.remote.api.PeerReviewMentorlyApi
 import com.sagrd.mentorly.data.remote.api.SubmissionMentorlyApi
 import com.sagrd.mentorly.data.remote.dto.EnumMoshiAdapters
-import com.sagrd.mentorly.data.remote.remotedatasource.PeerReviewRemoteDataSource
+import com.sagrd.mentorly.data.remote.remotedatasource.CourseRemoteDataSource
 import com.sagrd.mentorly.data.remote.remotedatasource.SubmissionRemoteDataSource
-import com.sagrd.mentorly.data.repository.PeerReviewRepositoryImpl
+import com.sagrd.mentorly.data.repository.AuthRepositoryImpl
+import com.sagrd.mentorly.data.repository.CourseRepositoryImpl
 import com.sagrd.mentorly.data.repository.SubmissionRepositoryImpl
-import com.sagrd.mentorly.domain.repository.PeerReviewRepository
+import com.sagrd.mentorly.domain.repository.AuthRepository
+import com.sagrd.mentorly.domain.repository.CourseRepository
 import com.sagrd.mentorly.domain.repository.SubmissionRepository
 import com.squareup.moshi.KotlinJsonAdapterFactory
 import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -23,7 +30,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-
     @Provides
     @Singleton
     fun provideMoshi(): Moshi {
@@ -57,13 +63,14 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun providePeerReviewApi(moshi: Moshi): PeerReviewMentorlyApi {
-        val baseUrl = "https://mentorlyapi-ap2-f8gfgwh3efchgzfn.eastus2-01.azurewebsites.net/"
-        return Retrofit.Builder()
-            .baseUrl(baseUrl)
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .build()
-            .create(PeerReviewMentorlyApi::class.java)
+    fun provideCourseRemoteDataSource(api: CourseMentorlyApi): CourseRemoteDataSource {
+        return CourseRemoteDataSource(api)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCourseRepository(remoteDataSource: CourseRemoteDataSource): CourseRepository {
+        return CourseRepositoryImpl(remoteDataSource)
     }
 
     @Provides
@@ -72,9 +79,4 @@ object AppModule {
         return SubmissionRepositoryImpl(remoteDataSource)
     }
 
-    @Provides
-    @Singleton
-    fun providePeerReviewRepository(remoteDataSource: PeerReviewRemoteDataSource): PeerReviewRepository {
-        return PeerReviewRepositoryImpl(remoteDataSource)
-    }
 }
